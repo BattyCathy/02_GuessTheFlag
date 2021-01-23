0b557bf9425334ce19cb1acffbabf01751a9e3e9
Mackenzie Overman <39047747+BattyCathy@users.noreply.github.com>
2021-01-22 16:26:39 -0800

    Initial Commit

---
+++ 02_GuessTheFlag/_2_GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  _2_GuessTheFlagApp.swift
+//  02_GuessTheFlag
+//
+//  Created by Guy on 1/22/21.
+//
+
+import SwiftUI
+
+@main
+struct _2_GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
